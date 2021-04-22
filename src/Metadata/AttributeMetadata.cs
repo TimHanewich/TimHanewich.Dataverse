62c65ea4b671b66346be6d7e706e91999a7b145c
Tim Hanewich <tahanewich@live.com>
2021-04-22 12:41:23 -0400

    Added ParseFromApiJson to AttributeMetadata

--- src/Metadata/AttributeMetadata.cs
+++ src/Metadata/AttributeMetadata.cs
@@ -1,4 +1,6 @@
 using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace TimHanewich.Cds.Metadata
 {
@@ -18,5 +20,28 @@ namespace TimHanewich.Cds.Metadata
         public string DisplayName {get; set;}
         public bool IsAuditEnabled {get; set;}
         public bool IsCustomizable {get; set;}
+
+        public AttributeMetadata ParseFromApiJson(string json)
+        {
+            JObject jo = JObject.Parse(json);
+            AttributeMetadata ToReturn = new AttributeMetadata();
+
+            ToReturn.MetadataId = Guid.Parse(jo.Property("MetadataId").Value.ToString());
+            ToReturn.AttributeType = jo.Property("AttributeType").Value.ToString();
+            ToReturn.ColumnNumber = Convert.ToInt32(jo.Property("ColumnNumber").Value.ToString());
+            ToReturn.IntroducedVersion = new Version(jo.Property("Version").Value.ToString());
+            ToReturn.IsManaged = Convert.ToBoolean(jo.Property("IsManaged").Value.ToString());
+            ToReturn.LogicalName = jo.Property("LogicalName").Value.ToString();
+            ToReturn.SchemaName = jo.Property("SchemaName").Value.ToString();
+            ToReturn.Format = jo.Property("Format").ToString();
+            ToReturn.MaxLength = Convert.ToInt32(jo.Property("MaxLength").Value.ToString());
+            ToReturn.DatabaseLength = Convert.ToInt32(jo.Property("DatabaseLength").Value.ToString());
+            ToReturn.Description = CdsServiceMetadataExtension.GetLocalizedLabel(jo, "Description");
+            ToReturn.DisplayName = CdsServiceMetadataExtension.GetLocalizedLabel(jo, "DisplayName");
+            ToReturn.IsAuditEnabled = CdsServiceMetadataExtension.GetNestedBoolean(jo, "IsAuditEnabled");
+            ToReturn.IsCustomizable = CdsServiceMetadataExtension.GetNestedBoolean(jo, "IsCustomizable");
+            
+            return ToReturn;
+        }
     }
 }
