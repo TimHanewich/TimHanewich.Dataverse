0d614edd1f456cdc260eb0321c32968134a16625
Tim Hanewich <tahanewich@live.com>
2021-04-22 14:57:05 -0400

    Added attribute type enum

---
+++ src/Metadata/AttributeType.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace TimHanewich.Cds.Metadata
+{
+    public enum AttributeType
+    {
+        Other = 0,
+        String = 1,
+        Money = 2,
+        Integer = 3,
+        Lookup = 4,
+        Boolean = 5,
+        DateTime = 6,
+        Memo = 7,
+        Decimal = 8,
+        Customer = 9,
+        Virtual = 10, //An image
+        Picklist = 11,
+        Double = 12,
+        BigInt = 13,
+        EntityName = 14,
+        State = 15,
+        Owner = 16,
+        Uniqueidentifier = 17,
+        Status = 18
+    }
+}
