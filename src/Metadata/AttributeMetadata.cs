bcf0486bf21b1b33322e407bf386d961002deabb
Tim Hanewich <tahanewich@live.com>
2021-04-22 12:51:37 -0400

    Seperated out string-specific values

--- src/Metadata/AttributeMetadata.cs
+++ src/Metadata/AttributeMetadata.cs
@@ -12,14 +12,17 @@ namespace TimHanewich.Cds.Metadata
         public Version IntroducedVersion {get; set;}
         public bool IsManaged {get; set;}
         public string LogicalName {get; set;}
-        public string SchemaName {get; set;}
-        public string Format {get; set;}
-        public int MaxLength {get; set;}
-        public int DatabaseLength {get; set;}
+        public string SchemaName {get; set;} 
         public string Description {get; set;}
         public string DisplayName {get; set;}
         public bool IsAuditEnabled {get; set;}
         public bool IsCustomizable {get; set;}
+
+        //String-specific values
+        //These will only be populated if the attribute derives from a string field in CDS
+        public string Format {get; set;}
+        public int MaxLength {get; set;}
+        public int DatabaseLength {get; set;}
 
         public static AttributeMetadata ParseFromApiJson(string json)
         {
@@ -33,13 +36,27 @@ namespace TimHanewich.Cds.Metadata
             ToReturn.IsManaged = Convert.ToBoolean(jo.Property("IsManaged").Value.ToString());
             ToReturn.LogicalName = jo.Property("LogicalName").Value.ToString();
             ToReturn.SchemaName = jo.Property("SchemaName").Value.ToString();
-            ToReturn.Format = jo.Property("Format").Value.ToString();
-            ToReturn.MaxLength = Convert.ToInt32(jo.Property("MaxLength").Value.ToString());
-            ToReturn.DatabaseLength = Convert.ToInt32(jo.Property("DatabaseLength").Value.ToString());
             ToReturn.Description = CdsServiceMetadataExtension.GetLocalizedLabel(jo, "Description");
             ToReturn.DisplayName = CdsServiceMetadataExtension.GetLocalizedLabel(jo, "DisplayName");
             ToReturn.IsAuditEnabled = CdsServiceMetadataExtension.GetNestedBoolean(jo, "IsAuditEnabled");
             ToReturn.IsCustomizable = CdsServiceMetadataExtension.GetNestedBoolean(jo, "IsCustomizable");
+
+            //String-related fields
+            JProperty prop_Format = jo.Property("Format");
+            if (prop_Format != null)
+            {
+                ToReturn.Format = jo.Property("Format").Value.ToString();
+            }
+            JProperty prop_MaxLength = jo.Property("MaxLength");
+            if (prop_MaxLength != null)
+            {
+                ToReturn.MaxLength = Convert.ToInt32(jo.Property("MaxLength").Value.ToString());
+            }
+            JProperty prop_DatabaseLength = jo.Property("DatabaseLength");
+            if (prop_DatabaseLength != null)
+            {
+                ToReturn.DatabaseLength = Convert.ToInt32(jo.Property("DatabaseLength").Value.ToString());
+            }
             
             return ToReturn;
         }
