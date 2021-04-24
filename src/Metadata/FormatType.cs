aa53a4f753c7a58c6718561f58a71a41b4ce1e2a
Tim Hanewich <tahanewich@live.com>
2021-04-24 14:30:51 -0400

    Added FormatType

---
+++ src/Metadata/FormatType.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace TimHanewich.Cds.Metadata
+{
+    public enum FormatType
+    {
+        Other = 0,
+        Text = 1,
+        DateAndTime = 2,
+        None = 3,
+        TextArea = 4,
+        Url = 5,
+        TimeZone = 6,
+        Duration = 7,
+        Language = 8,
+        DateOnly = 9,
+        Email = 10,
+        TickerSymbol = 11
+    }
+}
