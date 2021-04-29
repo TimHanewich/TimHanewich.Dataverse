5a37f3995d647dd3d170f1d94120ccee1b438a78
Tim Hanewich <tahanewich@live.com>
2021-04-28 21:00:41 -0400

    Added StringFormat enum

---
+++ src/Metadata/StringFormat.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace TimHanewich.Cds.Metadata
+{
+    public enum StringFormat
+    {
+        Other = 0,
+        Text = 1,
+        Url = 2,
+        TickerSymbol = 3,
+        Email = 4,
+        Phone = 5,
+        TextArea = 6
+    }
+}
