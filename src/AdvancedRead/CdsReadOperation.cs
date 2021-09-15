2c43027eb76550c8b70a2b0bf08cefe63e2b1e4f
Tim Hanewich <tahanewich@live.com>
2021-09-15 10:02:30 -0400

    Started advanced read

---
+++ src/AdvancedRead/CdsReadOperation.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace TimHanewich.Cds.AdvancedRead
+{
+    public class CdsReadOperation
+    {
+        public string Setter {get; set;}
+        public Guid? RecordId {get; set;}
+    }
+}
---
+++ src/AdvancedRead/ComparisonOperator.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TimHanewich.Cds.AdvancedRead
+{
+    public enum ComparisonOperator
+    {
+        Equals = 0,
+        NotEqualTo = 1,
+        GreaterThan = 2,
+        LessThan = 3,
+    }
+}
