1582e5fac8fc226d9e73c31961a82541ad16a70f
Tim Hanewich <tahanewich@live.com>
2023-03-21 13:06:14 -0400

    Added CdsToolkit

---
+++ src/CdsToolkit.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace TimHanewich.Cds
+{
+    public class CdsToolkit
+    {
+        // If you have a property called "cra0f_homepen" that is a lookup to another table, you can not use this exact name to do things like filters on the table.
+        // When you query data, it will look like "_cra0f_homepen_value". So this is what you need to use for things like filtering.
+        public static string LookupPropertyToLookupReference(string property_name)
+        {
+            return "_" + property_name + "_value";
+        }
+    }
+}
